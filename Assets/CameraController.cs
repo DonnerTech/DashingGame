1a77943cae9050a2aa66e9149f05949b0450cb67
DonnerTech <77709088+DonnerTech@users.noreply.github.com>
2025-07-24 18:38:07 -0700

    Camera Tweaks

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -6,6 +6,7 @@ public class CameraController : MonoBehaviour
     private NearestPointOnSpline nearestPointOnSpline;
     [SerializeField] private Player player;
     [SerializeField] private float tilt;
+    [SerializeField] private float rotSpeed = 1f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,8 +17,8 @@ public class CameraController : MonoBehaviour
     void Update()
     {
         //get vector the camera should face
-        Vector3 dir = nearestPointOnSpline.NearestTangent(player.transform.position);
-
+        Vector3 dir = Vector3.MoveTowards(transform.forward, nearestPointOnSpline.NearestTangent(player.transform.position), rotSpeed * Time.deltaTime);
+        dir.y = 0;
         //point the camera in the direction of the tangent
         transform.rotation = Quaternion.LookRotation(dir);
         transform.Rotate(new Vector3(tilt, 0, 0));
