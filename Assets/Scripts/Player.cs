e0e742bb9e85e12a41126a65908616d888ec3b98
DonnerTech <77709088+DonnerTech@users.noreply.github.com>
2025-07-24 17:36:44 -0700

    Player Nav Agent

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,35 +1,48 @@
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.InputSystem;
-using UnityEngine.Scripting;
 
-[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class Player : MonoBehaviour
 {
-    private CharacterController controller;
     [SerializeField] private float speed = 2.0f;
     [SerializeField] private float drag = 0.9f;
     [SerializeField] private float verticalPosition = 0f;
 
     private Transform model;
-    private Vector3 direction;
+    private Vector3 heading = Vector3.zero;
+    private Vector3 facing = Vector3.forward;
     private Vector3 velocity;
+
+    public NavMeshAgent navMeshAgent;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        // navMeshAgent = GetComponent<NavMeshAgent>();
+        // navMeshAgent.updatePosition = false;
+        navMeshAgent.updateRotation = false;
+        navMeshAgent.updateUpAxis = false;
+
         model = transform.GetChild(0);
     }
 
     void FixedUpdate()
     {
-        transform.position.Set(transform.position.x, verticalPosition, transform.position.z);
+        // transform.position = new Vector3(transform.position.x, verticalPosition, transform.position.z);
 
-        velocity += direction * speed;
+        // move the player by their input
+        velocity += InputCameraRemap(heading) * speed;
         velocity *= drag;
-        controller.Move(velocity);
 
-        // direction = Vector3.zero;
+        navMeshAgent.nextPosition = transform.position;
+        if (navMeshAgent.CalculatePath(transform.position + velocity, navMeshAgent.path))
+        {
+            navMeshAgent.Move(velocity);
+        }
+
+        //rotate the player by their input
+        model.forward = Vector3.Lerp(InputCameraRemap(facing), model.forward,1 - Vector3.Angle(InputCameraRemap(facing), model.forward)/120);
     }
 
     //Remaps input to move in the direction the camera faces
@@ -41,13 +54,14 @@ public class Player : MonoBehaviour
         return Quaternion.AngleAxis(rotation, Vector3.up) * input;
     }
 
+    //recieve heading inputs
     public void OnMove(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
-        direction = new Vector3(input.x, 0, input.y);
-        direction = InputCameraRemap(direction);
+        heading = new Vector3(input.x, 0, input.y);
     }
 
+    //recieve facing inputs
     public void OnLook(InputAction.CallbackContext context)
     {
         Vector2 input = context.ReadValue<Vector2>();
@@ -56,7 +70,7 @@ public class Player : MonoBehaviour
         //face the player model in the look direction
         if (lookDir != Vector3.zero)
         {
-            model.forward = InputCameraRemap(lookDir.normalized);
+            facing = lookDir.normalized;
         }
     }
 
